e76102275bfa19655b03b5c7d31f75a2a16db8f3
wsldev <wslmacieira@gmail.com>
2023-08-27 12:39:38 -0300

    feat: add base class entities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from "node:crypto"
+import { Entity } from "../../core/entities/entity"
 import { Slug } from './value-objects/slug'
 
 interface QuestionProps {
@@ -8,18 +8,4 @@ interface QuestionProps {
   authorId: string
 }
 
-export class Question {
-  public id: string
-  public title: string
-  public content: string
-  public slug: Slug
-  public authorId: string
-
-  constructor(props: QuestionProps, id?: string) {
-    this.title = props.title
-    this.content = props.content
-    this.slug = props.slug
-    this.authorId = props.authorId
-    this.id = id ?? randomUUID()
-  }
-}
+export class Question extends Entity<QuestionProps> { }
