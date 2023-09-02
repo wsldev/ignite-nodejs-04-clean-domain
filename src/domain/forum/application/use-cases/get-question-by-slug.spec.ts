5350f2aa952e6181f5f43b30b02b2539e8d381ab
wsldev <wslmacieira@gmail.com>
2023-09-02 12:30:07 -0300

    feat: add getQuestionBySlug useCase

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,33 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { Question } from '../../enterprise/entities/question'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = Question.create({
+      authorId: new UniqueEntityID('1'),
+      title: 'Example question',
+      slug: Slug.create('example-question'),
+      content: 'Example content',
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toEqual(newQuestion.title)
+  })
+})
