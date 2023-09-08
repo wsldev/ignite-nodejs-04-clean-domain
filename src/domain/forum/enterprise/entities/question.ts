1ab36d2d75d2ebbf7463a1073be2c8db4e997b31
Wagner dos Santos Lima <wslmacieira@gmail.com>
2023-09-08 02:22:09 -0300

    feat: add fetchRecentQuestionsUseCase

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -79,7 +79,7 @@ export class Question extends Entity<QuestionProps> {
       {
         ...props,
         slug: props.slug ?? Slug.createFromtext(props.title),
-        createdAt: new Date(),
+        createdAt: props.createdAt ?? new Date(),
       },
       id,
     )
